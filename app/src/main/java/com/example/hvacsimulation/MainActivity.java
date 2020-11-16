6ffc910d79504c5839a14c2134659cbfb6573295
ESanchezCSE <ESanchez.CSE20@gmail.com>
2020-11-15 23:18:37 -0600

    App automatically updates when the real database is updated.

--- app/src/main/java/com/example/hvacsimulation/MainActivity.java
+++ app/src/main/java/com/example/hvacsimulation/MainActivity.java
@@ -3,12 +3,74 @@ package com.example.hvacsimulation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Button getInfoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Button
+        getInfoButton = (Button) findViewById(R.id.getInfoBTN);
+
+        getInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //basicReadWrite();
+            }
+        });
+
+        basicReadWrite();
+
+    }
+
+    public void updateCurrTemp(String toThis) {
+        TextView textView = (TextView) findViewById(R.id.currTempText);
+        textView.setText(toThis);
+    }
+
+    public void basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("home");
+
+        //myRef.setValue("Hello, World!");
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.child("currOutsideTemp").getValue().toString();
+                Log.d(TAG, "Value is: " + value);
+                updateCurrTemp(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        // [END read_message]
     }
 }
